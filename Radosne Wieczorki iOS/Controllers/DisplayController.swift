2387252f77827f85a346ca62cf55f530a1848637
tomsloj <tomsloj@gmail.com>
2022-05-06 15:21:09 +0200

    add links in descriptions

--- Radosne Wieczorki iOS/Controllers/DisplayController.swift
+++ Radosne Wieczorki iOS/Controllers/DisplayController.swift
@@ -13,7 +13,8 @@ class DisplayController: UIViewController {
     
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet weak var categoryLabel: UILabel!
-    @IBOutlet weak var textLabel: UILabel!
+//    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textLabel: UITextView!
     var gameName:String?
     
     let sService:SettingsService = SettingsService()
@@ -29,10 +30,15 @@ class DisplayController: UIViewController {
         categoryLabel.text = "Kategoria: " + databaseHelper.getCategory(game: gameName!)
         
         textLabel.text = databaseHelper.getText(game: gameName!)
+        textLabel.linkTextAttributes = [.foregroundColor: UIColor.systemBlue]
+        textLabel.isSelectable = true
+        textLabel.isEditable = false
+        textLabel.isUserInteractionEnabled = true
+        textLabel.dataDetectorTypes = .link
         
         titleLabel.font = titleLabel.font.withSize(CGFloat(sService.getTextSize() + 4.0))
         categoryLabel.font = categoryLabel.font.withSize(CGFloat(sService.getTextSize()))
-        textLabel.font = textLabel.font.withSize(CGFloat(sService.getTextSize()))
+        textLabel.font = textLabel.font?.withSize(CGFloat(sService.getTextSize()))
     }
         
     override func viewWillAppear(_ animated: Bool) {
@@ -41,7 +47,7 @@ class DisplayController: UIViewController {
         
         titleLabel.font = titleLabel.font.withSize(CGFloat(sService.getTextSize() + 4.0))
         categoryLabel.font = categoryLabel.font.withSize(CGFloat(sService.getTextSize()))
-        textLabel.font = textLabel.font.withSize(CGFloat(sService.getTextSize()))
+        textLabel.font = textLabel.font?.withSize(CGFloat(sService.getTextSize()))
     }
     
     @IBAction func addToList(_ sender: Any) {
